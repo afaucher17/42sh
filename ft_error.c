c8eadefb9a7a5177e338db26dd0b3492fc105ecb
Thomas DIEUMEGARD <tdieumeg@e3r9p7.42.fr>
2014-03-07 18:37:28 +0100

    CHINOIS bis

--- ft_error.c
+++ ft_error.c
@@ -6,17 +6,17 @@
 /*   By: tdieumeg <tdieumeg@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/03/05 16:46:20 by tdieumeg          #+#    #+#             */
-/*   Updated: 2014/03/05 16:51:58 by tdieumeg         ###   ########.fr       */
+/*   Updated: 2014/03/07 17:44:36 by tdieumeg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "42sh.h"
 
-void		ft_error(char *filename, char *error)
+int			ft_error(char *filename, char *error)
 {
 	ft_putstr_fd("42sh: ", 2);
 	ft_putstr_fd(filename, 2);
 	ft_putstr_fd(": ", 2);
 	ft_putendl_fd(error, 2);
-	exit(EXIT_FAILURE);
+	return (0);
 }
--- ft_token_handler.c
+++ ft_token_handler.c
@@ -6,36 +6,11 @@
 /*   By: tdieumeg <tdieumeg@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/01/25 13:06:24 by tdieumeg          #+#    #+#             */
-/*   Updated: 2014/03/05 16:35:08 by tdieumeg         ###   ########.fr       */
+/*   Updated: 2014/03/07 13:01:48 by tdieumeg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include		"42sh.h"
-
-char			**ft_arg_handler(t_node *tree, char *cmd)
-{
-	char		**com;
-	t_node		*save;
-	int			i;
-
-	if (!cmd)
-		return (NULL);
-	save = tree;
-	i = 0;
-	while ((save = save->right) != NULL)
-		i++;
-	com = (char**)malloc(sizeof(char*) * (i + 2));
-	i = 1;
-	com[0] = ft_strdup(cmd);
-	free(cmd);
-	while ((tree = tree->right) != NULL)
-	{
-		com[i] = ft_strdup(tree->data);
-		i++;
-	}
-	com[i] = NULL;
-	return (com);
-}
 
 static int		ft_pipe_handler(t_node *tree, t_list **env, int *pfd)
 {
@@ -55,7 +30,6 @@ static void		ft_logic_handler(t_node *tree, t_list **env, int ret, int op)
 {
 	int			ret2;
 	t_node		*node;
-
 	
 	if (tree->type == COM || tree->type == PIPE_EXP)
 		node = tree;
