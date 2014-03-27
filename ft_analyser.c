86e3cbdd1015f00be05adfb07f9abf059fb06b02
Julien LINDEN <julien.linden@free.fr>
2014-03-27 15:33:09 +0100

    Juju norme 4

--- ft_analyser.c
+++ ft_analyser.c
@@ -6,7 +6,7 @@
 /*   By: tdieumeg <tdieumeg@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/02/26 18:19:05 by tdieumeg          #+#    #+#             */
-/*   Updated: 2014/03/19 15:27:53 by jlinden          ###   ########.fr       */
+/*   Updated: 2014/03/27 15:31:35 by jlinden          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,30 +29,42 @@ static char		*ft_wait_finish(char *str, t_mlist *mlist)
 	return (complete);
 }
 
+static int		ft_son_init(int spec[5], int *count)
+{
+	int		i;
+
+	i = 0;
+	*count = -1;
+	while (i < 5)
+	{
+		spec[i] = 0;
+		i++;
+	}
+	return (1);
+}
+
 char			*ft_son(char *cmd, t_mlist *mlist)
 {
 	int			i;
-	int			special[6] = {0, 0, 0, 0, 0, 0};
+	int			spec[5];
 
-	i = 0;
-	while (cmd[i])
+	ft_son_init(spec, &i);
+	while (cmd[++i])
 	{
 		if (cmd[i] == '\\' && (i = i + 2))
 			continue ;
-		special[0] ^= (cmd[i] == '\"' && !special[1]);
-		special[1] ^= (cmd[i] == '\'' && !special[0]);
-		special[2] ^= (cmd[i] == '`' && !special[1]);
-		if (!special[0] && !special[1])
+		spec[0] ^= (cmd[i] == '\"' && !spec[1]);
+		spec[1] ^= (cmd[i] == '\'' && !spec[0]);
+		spec[2] ^= (cmd[i] == '`' && !spec[1]);
+		if (!spec[0] && !spec[1])
 		{
-			special[3] -= (cmd[i] == ')');
-			special[3] += (cmd[i] == '(');
-			special[4] -= (cmd[i] == ']');
-			special[4] += (cmd[i] == '[');
+			spec[3] -= (cmd[i] == ')');
+			spec[3] += (cmd[i] == '(');
+			spec[4] -= (cmd[i] == ']');
+			spec[4] += (cmd[i] == '[');
 		}
-		i++;
 	}
-	if (special[0] || special[1] || special[2]
-		|| special[3] > 0 || special[4] > 0)
+	if (spec[0] || spec[1] || spec[2] || spec[3] > 0 || spec[4] > 0)
 	{
 		cmd = ft_wait_finish(cmd, mlist);
 		return (ft_son(cmd, mlist));
@@ -60,18 +72,34 @@ char			*ft_son(char *cmd, t_mlist *mlist)
 	return (cmd);
 }
 
+static char		*ft_anal_father(int pfd[2])
+{
+	int		len;
+	char	buf[BUFF_SIZE + 1];
+
+	signal(SIGINT, ft_sighand2);
+	close(pfd[1]);
+	dup2(pfd[0], 0);
+	while ((len = read(0, buf, BUFF_SIZE)) > 0)
+		buf[len] = '\0';
+	wait(NULL);
+	close(pfd[0]);
+	dup2((ft_reset_std())[0], 0);
+	if (buf[0] == '\0')
+		return (NULL);
+	return (ft_strdup(buf));
+}
+
 char			*ft_analyser(char *cmd, t_mlist *mlist)
 {
 	int			pid;
 	char		*res;
 	char		buf[BUFF_SIZE];
-	int			len;
 	int			pfd[2];
 
 	ft_bzero(buf, BUFF_SIZE);
 	pipe(pfd);
-	pid = fork();
-	if (pid == 0)
+	if ((pid = fork()) == 0)
 	{
 		signal(SIGINT, SIG_DFL);
 		close(pfd[0]);
@@ -82,18 +110,6 @@ char			*ft_analyser(char *cmd, t_mlist *mlist)
 		exit(EXIT_SUCCESS);
 	}
 	else
-	{
-		signal(SIGINT, ft_sighand2);
-		close(pfd[1]);
-		dup2(pfd[0], 0);
-		while ((len = read(0, buf, BUFF_SIZE - 1)) > 0)
-			buf[len] = '\0';
-		wait(NULL);
-		close(pfd[0]);
-		dup2((ft_reset_std())[0], 0);
-		if (buf[0] == '\0')
-			return (NULL);
-		return (ft_strdup(buf));
-	}
+		return (ft_anal_father(pfd));
 	return (NULL);
 }
